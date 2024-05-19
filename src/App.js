688e1da9bf7cf156f895f7f652467dfeb7ae1260
TungDuong1202 <phanduong122004@gmail.com>
2024-05-19 12:43:23 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { MdDirectionsBoatFilled } from "react-icons/md";
+import { FaBars } from "react-icons/fa6";
+import { IoIosCloseCircle } from "react-icons/io";
+import "./navbar.css";
+
+const Navbar = () => {
+  const [active, setActive] = useState("navBar");
+  const showNavBar = () => {
+    setActive("navBar activeNavbar");
+  };
+  const closeNavBar = () => {
+    setActive("navBar");
+  };
+  return (
+    <section className="navBarSection">
+      <header className="header">
+        <div className="logoDiv">
+          <a href="#" className="logo">
+            <MdDirectionsBoatFilled className="icon" />
+            <h2>Travel</h2>
+          </a>
+        </div>
+
+        <div className={active}>
+          <ul className="navLists">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <button className="btn">
+              <a href="#">Liên hệ Group3</a>
+            </button>
+          </ul>
+          <div onClick={closeNavBar} className="closeNavBar">
+            <IoIosCloseCircle className="icon" />
+          </div>
+        </div>
+
+        <div onClick={showNavBar} className="tagNavBar">
+          <FaBars className="icon" />
+        </div>
+      </header>
+    </section>
+  );
+};
+
+export default Navbar;
