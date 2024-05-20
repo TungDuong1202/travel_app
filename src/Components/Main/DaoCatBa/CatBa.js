7a877553fa9bf059ab0a26a6e3ef159f6f7ab56d
Ngohoainam204 <146152997+Ngohoainam204@users.noreply.github.com>
2024-05-20 22:16:13 +0700

    Add files via upload

---
+++ src/Components/Main/DaoCatBa/CatBa.js
@@ -0,0 +1,160 @@
+import React from "react";
+import "./css.css";
+import { FaShip } from "react-icons/fa6";
+import { FaAnchor } from "react-icons/fa6";
+import { MdBathroom } from "react-icons/md";
+const DaoCatBa = () => {
+    return (
+<main>
+
+<section class="py-5 text-center container">
+  <div class="row py-lg-5">
+    <div class="col-lg-6 col-md-8 mx-auto">
+      <h1 class="fw-light">Thông tin về du thuyền Đảo Cát Bà</h1>
+      <p class="Direc2">Tận hưởng kỳ nghỉ đáng nhớ tại Cát Bà - hòn đảo lớn nhất tại vịnh Hạ Long với hàng loạt trải nghiệm thú vị.</p>
+      
+    </div>
+  </div>
+</section>
+
+<div class="album py-5 bg-body-tertiary">
+  <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div class="col">
+        
+        <div class="card shadow-sm">
+          <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5b5e317c7d3.jpg" alt=""/>
+          <div class="card-body">
+            <h3>Du thuyền Hiritage</h3>
+            <ul class="card-text">
+              <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+            </ul>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+              </div>
+              <small class="text-body-secondary">5,300,000đ / khách</small>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <div class="col">
+        
+          <div class="card shadow-sm">
+          <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5a5c80c7c94.jpg" alt=""/>
+            <div class="card-body">
+              <h3>Du thuyền Orchid</h3>
+              <ul class="card-text">
+               <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+                </div>
+                <small class="text-body-secondary">5,300,000đ / khách</small>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+      <div class="col">  
+          <div class="card shadow-sm">
+            <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5b5d636e236.jpg" alt=""/>
+            <div class="card-body">
+              <h3>Du thuyền Azalea</h3>
+              <ul class="card-text">
+               <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+                </div>
+                <small class="text-body-secondary">5,300,000đ / khách</small>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+      <div class="col">
+        
+          <div class="card shadow-sm">
+            <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5a6148cbb4b.jpg" alt=""/>
+            <div class="card-body">
+              <h3>Du thuyền Mon Cheri</h3>
+              <ul class="card-text">
+               <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+                  
+                </div>
+                <small class="text-body-secondary">5,300,000đ / khách</small>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+      <div class="col">
+        
+          <div class="card shadow-sm">
+            <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5a72f08a2a0.jpg" alt=""/>
+            <div class="card-body">
+              <h3>Du thuyền Era</h3>
+              <ul class="card-text">
+               <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+                  
+                </div>
+                <small class="text-body-secondary">5,300,000đ / khách</small>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+      <div class="col">
+        
+          <div class="card shadow-sm">
+            <img class ="anhminhhoa" src="https://owa.bestprice.vn/images/articles/uploads/top-5-du-thuyen-cat-ba-noi-tieng-nhat-5d5b5e317c7d3.jpg" alt=""/>
+            <div class="card-body">
+              <h3>Du thuyền La Regina</h3>
+              <ul class="card-text">
+               <li><FaShip/> Hạ thủy 2019</li>
+              <li><FaAnchor/> Vỏ tàu Kim loại</li>
+              <li><MdBathroom/> Số lượng: 20 phòng</li>
+              </ul>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Đặt ngay</button>
+                  
+                </div>
+                <small class="text-body-secondary">5,300,000đ / khách</small>
+              </div>
+            </div>
+          </div>
+          
+      </div>
+
+
+</div>
+</div>
+/</div>
+</main>
+
+    )
+}
+export default DaoCatBa 
